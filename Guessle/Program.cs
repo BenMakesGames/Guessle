d250a90a59d7502d05a0a4c97b5595aa639ce256
Ben <ben@benmakesgames.com>
2023-03-25 19:49:45 -0400

    initial (and probably only) commit

---
+++ Guessle/Program.cs
@@ -0,0 +1,195 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+const char RightLetterRightPlace = '+';
+const char RightLetterWrongPlace = '?';
+const char WrongLetter = '.';
+const string AllTheLetters = "abcdefghijklmnopqrstuvwxyz";
+var fiveLetterWords = GetFiveLetterWords("Content/dictionary.txt");
+
+var guessHistory = new GuessHistory();
+
+ShowHowToPlay();
+
+while (true)
+{
+    Console.Write("> ");
+
+    var guess = Console.ReadLine()?.Trim().ToLowerInvariant() ?? "";
+
+    if (guess == "quit")
+        break;
+    else if (guess == "hint")
+        ShowHints(guessHistory, fiveLetterWords);
+    else if (guess == "restart")
+    {
+        guessHistory = new GuessHistory();
+    }
+    else if (guess.Length == 5)
+    {
+        if (guess.Any(l => !AllTheLetters.Contains(l)))
+        {
+            Console.WriteLine("A guess must only contain letters.");
+            continue;
+        }
+
+        Console.WriteLine("What was WORDLE's response? Type 5 characters using the following rules:");
+        Console.WriteLine($"{RightLetterRightPlace} for a letter in the correct position");
+        Console.WriteLine($"{RightLetterWrongPlace} for a letter in the wrong position");
+        Console.WriteLine($"{WrongLetter} for a letter not in the word");
+        Console.WriteLine("Type nothing to cancel.");
+
+        while (true)
+        {
+            Console.Write("> ");
+
+            var response = Console.ReadLine()?.Trim().ToLowerInvariant() ?? "";
+
+            if (response.Length == 0)
+                break;
+
+            if (response.Length != 5)
+            {
+                Console.WriteLine("Please enter 5 characters.");
+                continue;
+            }
+
+            if (!response.Any(r => r is not RightLetterRightPlace or RightLetterWrongPlace or WrongLetter))
+            {
+                Console.WriteLine($"Only {RightLetterRightPlace}, {RightLetterWrongPlace}, and {WrongLetter} should be used.");
+                continue;
+            }
+
+            Console.WriteLine($"Got it. You guessed \"{guess}\", and:");
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{guess[i]} is {DescribeResponse(response[i])}");
+            }
+
+            guessHistory.Guesses.Add(new(guess, response));
+            break;
+        }
+    }
+    else
+    {
+        ShowHowToPlay();
+    }
+}
+
+static string DescribeResponse(char c) => c switch
+{
+    '+' => "correct",
+    '?' => "in the wrong place",
+    '.' => "not in the word at all",
+    _ => throw new UnreachableException()
+};
+
+static List<string> GetFiveLetterWords(string fileName)
+{
+    var words = new List<string>();
+
+    using var reader = new StreamReader(fileName);
+
+    while (reader.ReadLine()?.Trim() is {} line)
+    {
+        if (line.Length == 5)
+            words.Add(line.ToLowerInvariant());
+    }
+
+    return words;
+}
+
+static void ShowHowToPlay()
+{
+    Console.WriteLine("Enter a five letter word to register a guess. You will then be asked for WORDLE's response.");
+    Console.WriteLine("Type \"hint\" to get a list of words that the solution COULD be.");
+    Console.WriteLine("Type \"restart\" to restart the game.");
+    Console.WriteLine("Type \"quit\" to quit.");
+}
+
+static void ShowHints(GuessHistory guessHistory, List<string> allFiveLetterWords)
+{
+    var possibleWords = allFiveLetterWords
+        .Where(w => WordIsPossible(w, guessHistory))
+        .ToList();
+
+    Console.WriteLine($"There are {possibleWords.Count} possible words.");
+    Console.WriteLine("Show them now? (Y/n)");
+
+    var showThem = GetYesOrNo();
+
+    if (!showThem)
+    {
+        Console.WriteLine("Okay.");
+        return;
+    }
+
+    Console.WriteLine(string.Join(Environment.NewLine, possibleWords));
+}
+
+static bool WordIsPossible(string word, GuessHistory wordInformation)
+    => wordInformation.Guesses.All(guess => WordMatchesGuess(word, guess));
+
+static bool WordMatchesGuess(string word, Guess guess)
+{
+    if(WordContainsAnyWrongLetters(word, guess))
+        return false;
+
+    // all right-letter-right-place letters must be accounted for
+    for (int i = 0; i < 5; i++)
+    {
+        if(guess.Response[i] == RightLetterRightPlace && word[i] != guess.Word[i])
+            return false;
+    }
+
+    if (!WordContainsAllRequiredLetters(word, guess))
+        return false;
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (word[i] == guess.Word[i] && guess.Response[i] != RightLetterRightPlace)
+            return false;
+    }
+
+    return true;
+}
+
+static bool WordContainsAnyWrongLetters(string word, Guess guess)
+    => word.Any(l => guess.Word.Contains(l) && guess.Response[guess.Word.IndexOf(l)] == WrongLetter);
+
+static bool WordContainsAllRequiredLetters(string word, Guess guess)
+{
+    var remainingLetters = guess.Word.Where((_, i) => guess.Response[i] != WrongLetter).ToList();
+
+    for (int i = 0; i < 5 && remainingLetters.Count > 0; i++)
+    {
+        if(remainingLetters.IndexOf(word[i]) >= 0)
+            remainingLetters.RemoveAt(remainingLetters.IndexOf(word[i]));
+    }
+
+    return remainingLetters.Count == 0;
+}
+
+static bool GetYesOrNo()
+{
+    while (true)
+    {
+        Console.Write("> ");
+
+        var response = Console.ReadLine()?.Trim().ToLowerInvariant() ?? "";
+
+        if (response is "y" or "yes")
+            return true;
+        else if (response is "" or "n" or "no")
+            return false;
+    }
+}
+
+public class GuessHistory
+{
+    public List<Guess> Guesses { get; } = new();
+}
+
+public record Guess(string Word, string Response);
---
+++ README.md
@@ -0,0 +1,19 @@
+I like playing Wordle, but I get bored/annoyed if a game gets to the point where I've entered several guesses, and I know there's only a couple possible words that are valid guesses, I just can't think of them. There's some tipping point where some games stop feeling like I'm a human making fun decisions, and start feeling like I'm a dictionary trying to filter down to what words are legitimate guesses. And I'm not personally interested in being a dictionary... that's what computers are for.
+
+SO: I did the fun human thing of writing a program to be a boring dictionary for me, and I called it Guessle.
+
+Guessle doesn't make any attempt to suggest a good/smart guess - that's the fun human part I want to do - it JUST does the dictionary lookup to tell you what words are possible, based on the words you've guessed so far.
+
+---
+
+P.S. Guessle's `dictionary.txt` of 466k words was acquired from https://github.com/dwyl/english-words. I specifically chose to NOT use a dictionary of words known to be used by Wordle, because, again, Guessle's only job, for me, is to remove the burden of being a dictionary; not to play Wordle, or even really know anything about Wordle (Guessle just happens to let you look up words in a way that's helpful when playing Wordle).
+
+---
+
+P.P.S. There are surely more-efficient ways to do some of the search logic, but it's all going fast enough that my human brain doesn't notice, and this whole project is all about satisfying my human brain, so I'm good with that :P
+
+---
+
+P.P.P.S. my last name is not Guessle, but it is approximately as ridiculous.
+
+[![Buy Me a Coffee at ko-fi.com](https://raw.githubusercontent.com/BenMakesGames/AssetsForNuGet/main/buymeacoffee.png)](https://ko-fi.com/A0A12KQ16)
